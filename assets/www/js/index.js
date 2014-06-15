38b26f6ae00af6a9ffe79fc7171522dd013f0dd5
drorelg <dror.elg@gmail.com>
2014-06-15 21:22:27 +0300

    no message

--- assets/www/js/index.js
+++ assets/www/js/index.js
@@ -1,22 +1,4 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
-var app = {
+var main = {
     // Application Constructor
     initialize: function() {
         this.bindEvents();
@@ -45,5 +27,17 @@ var app = {
         receivedElement.setAttribute('style', 'display:block;');
 
         console.log('Received Event: ' + id);
-    }
+    },
+	addNotification : function(inID,inTitle,inDescription,inTime,inDate) {
+		// Create a new Date object according to the user's input	
+	var now            		= new Date().getTime()
+   	var notificationDate 	= new Date(inDate + ' ' + inTime); 			
+		
+	window.plugin.notification.local.add({
+		    id:      inID,
+		  	title:   inTitle,
+		    message: inDescription,
+		    date:    notificationDate
+		});
+					}
 };
